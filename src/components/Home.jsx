87ecee55c939e8863b9d4bc25496921af8b81638
Ricardo Salazar <ricardosalazar@Ricardos-MacBook-Pro.local>
2023-06-13 21:32:41 -0600

    images showed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,20 @@
+import axios from "axios";
 import React, { useState, useEffect } from "react";
+import getConfig from "../utils/getConfig";
 
 const Home = () => {
   const [time, setTime] = useState(new Date());
+  const [cats,setCats] = useState([])
 
   useEffect(() => {
+
+    axios.get('https://api.thecatapi.com/v1/breeds?limit=5&page=0',getConfig())
+    .then(res=>{
+        setCats(res.data)
+        console.log(cats[0]?.image);
+        console.log(cats);
+    })
+
     const timer = setInterval(() => {
       setTime(new Date());
     }, 1000);
@@ -12,13 +23,18 @@ const Home = () => {
       clearInterval(timer);
     };
   }, []);
-
   return (
     <div className="home-container">
       <div className="clock-container">
         <p>{time.toLocaleTimeString()}</p>
       </div>
-
+      <div className="images-container">
+        {
+            cats.map(cat=>(
+                <img src={cat.image?.url} alt="" key={cat.id}/>
+            ))
+        }
+      </div>
     </div>
   );
 };
---
+++ src/utils/getConfig.jsx
@@ -0,0 +1,5 @@
+const getConfig = () => ({
+    headers: { 'x-api-key': `live_3XdQvjXHaB3YynD26qNJarcy1GgwgzVViXcaFBRRfTIh4QCBjQnNVseUJj2adYcZ`}
+});
+
+export default getConfig;
