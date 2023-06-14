b01356f3dabf3b35680cde54dcb635456a12a5bc
Ricardo Salazar <ricardosalazar@Ricardos-MacBook-Pro.local>
2023-06-14 00:00:43 -0600

    design v1

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,32 @@
-import axios from "axios";
+import Slider from "react-slick";
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 import getConfig from "../utils/getConfig";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { useSelector } from 'react-redux';
 
 const Home = () => {
+  const [cats, setCats] = useState([]);
   const [time, setTime] = useState(new Date());
-  const [cats,setCats] = useState([])
+  const userName = useSelector(state => state.username);
+  console.log(userName);
 
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+  };
   useEffect(() => {
-
-    axios.get('https://api.thecatapi.com/v1/breeds?limit=5&page=0',getConfig())
-    .then(res=>{
-        setCats(res.data)
+    axios
+      .get("https://api.thecatapi.com/v1/breeds?limit=10&page=0", getConfig())
+      .then((res) => {
+        setCats(res.data);
         console.log(cats[0]?.image);
         console.log(cats);
-    })
+      });
 
     const timer = setInterval(() => {
       setTime(new Date());
@@ -24,16 +37,20 @@ const Home = () => {
     };
   }, []);
   return (
-    <div className="home-container">
+    <div>
       <div className="clock-container">
-        <p>{time.toLocaleTimeString()}</p>
+        <span>{`Bienvenido ${userName}`}</span>
+        <span>{time.toLocaleTimeString()}</span>
       </div>
-      <div className="images-container">
-        {
-            cats.map(cat=>(
-                <img src={cat.image?.url} alt="" key={cat.id}/>
-            ))
-        }
+
+      <div className="images-main-container">
+      <Slider {...settings}>
+        {cats.map((cat) => (
+          <div className="image-container">
+            <img src={cat.image.url} alt="" className="cat-image"/>
+          </div>
+        ))}
+      </Slider>
       </div>
     </div>
   );
