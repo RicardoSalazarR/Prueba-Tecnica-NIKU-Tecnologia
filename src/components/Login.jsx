2b3dfae3ceb5e0742dfdd00deb9977c684a78d57
Ricardo Salazar <ricardosalazar@Ricardos-MacBook-Pro.local>
2023-06-13 18:32:34 -0600

    json with users data added

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,14 +1,56 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "boxicons";
 import { useForm } from "react-hook-form";
+import axios from "axios";
 
 const Login = () => {
   const { handleSubmit } = useForm();
   const [showPass, setShowPass] = useState(false);
-  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await axios.get("../../public/data/users.json");
+        setUsers(response.data.users);
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      }
+    };
+
+    fetchUsers();
+  }, []);
 
   const login = () => {
-   
+    let valid = true;
+    let message = "";
+    if (!email) {
+      valid = false;
+      message = "Por favor introduzca un correo electrónico";
+    } else if (!pass) {
+      valid = false;
+      message = "Por favor intruzca una contraseña";
+    }
+
+    let usr = users.find((user) => user.email === email);
+
+    if (!usr) {
+      valid = false;
+      return alert("Correo electrónico invalido");
+    }
+
+    if(usr.password!== pass){
+        valid = false
+        return alert('Contraseña invalida')
+    }
+
+    if (valid) {
+      alert(`Bienvenido ${usr.name}`)
+    } else {
+      return alert(message);
+    }
   };
 
   const change = () => {
@@ -17,14 +59,16 @@ const Login = () => {
 
   return (
     <div className="login-container">
-      <p className="gray">Hola, por favor ingresa tus datos para iniciar sesión</p>
+      <p className="gray">
+        Hola, por favor ingresa tus datos para iniciar sesión
+      </p>
       <form action="" className="login-form" onSubmit={handleSubmit(login)}>
         <input
           type="text"
-          placeholder="Usuario ó email"
+          placeholder="Correo electrónico"
           className="form-input"
-          onChange={(e) => setUsername(e.target.value)}
-          value={username}
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
         />
         <div className="wrapp-input">
           <span className="show-icon" onClick={change}>
@@ -34,6 +78,8 @@ const Login = () => {
             type={showPass ? "text" : "password"}
             placeholder="Contraseña"
             className="form-input"
+            onChange={(e) => setPass(e.target.value)}
+            value={pass}
           />
         </div>
         <button className="login-button primary-button">Iniciar sesion</button>
